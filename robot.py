167e1eb7d31a86ad30037089f1fbdb87ab2e0dc8
Javier Cuan-Martinez <jcuanm@gmail.com>
2017-11-25 18:41:09 -0800

    Implemented early stopping of robot when it has already fallen

--- robot.py
+++ robot.py
@@ -1,7 +1,16 @@
 import gym
+NUM_EPISODES = 1000
 env = gym.make('Humanoid-v1')
-env.reset()
 
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
+for _ in range(NUM_EPISODES):
+    observation = env.reset()
+
+    for t in range(1000):
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info =  env.step(action)
+
+        # if our walker has fallen, end the episode
+        if done:
+            print("Episode finished after {} timesteps".format(t + 1))
+            break
