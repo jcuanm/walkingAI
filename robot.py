e5e46a995c36ea8d55546ef8ee300065bc1ebd61
Javier Cuan-Martinez <jcuanm@gmail.com>
2017-11-27 12:59:20 -0800

    Remove unnecessary print statement

--- robot.py
+++ robot.py
@@ -1,15 +1,15 @@
 import gym
 NUM_EPISODES = 1000
+TIME_STEPS = 1000
 env = gym.make('Walker2d-v1')
 
 def main():
     for _ in range(NUM_EPISODES):
         observation = env.reset()
 
-        for t in range(1000):
+        for t in range(TIME_STEPS):
             env.render()
             action = env.action_space.sample()
-            print (env.action_space)
             observation, reward, done, info =  env.step(action)
 
             # if our walker has fallen, end the episode
